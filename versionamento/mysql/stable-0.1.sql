1a8a6253894421c618f0a4726cf118432ba404fc
vitorsoratto <sorattoemail@gmail.com>
2021-06-09 21:15:15 -0300

    adicionado a tabela de telefoe

--- versionamento/mysql/stable-0.1.sql
+++ versionamento/mysql/stable-0.1.sql
@@ -4,3 +4,12 @@ CREATE TABLE pessoa (
     sobrenome VARCHAR(220),
     idade INT NOT NULL
 ) ENGINE = InnoDB;
+
+CREATE TABLE telefone (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    numero VARCHAR(255),
+    tipo VARCHAR(255),
+    pessoa_id INT
+) ENGINE = InnoDB;
+
+ALTER TABLE telefone ADD CONSTRAINT fk_telefone_pessoa FOREIGN KEY (pessoa_id) REFERENCES pessoa(id);
